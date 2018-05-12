df125189d9846bb122b761dd509e2e0052fc3c42
robertrancz <rancz.robi@gmail.com>
2018-05-12 20:54:17 +0300

    Add project files.

---
+++ UnifiApiDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UnifiApiDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
